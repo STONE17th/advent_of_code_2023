4027cbea5f30148860bf855ceb31caf2e035a4a1
STONE <17th@inbox.ru>
2023-12-09 11:54:02 +0300

    Day_09_solved

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
-def data_reader(path: str) -> tuple[str, dict[str, tuple[str, str]]]:
-    with open(path, 'r', encoding='UTF-8') as file:
-        data = list(map(lambda x: x.strip(), file.readlines()))
-    path_data = data[0].replace('L', '0').replace('R', '1')
-    result_map = {}
-    for i in range(2, len(data)):
-        key, value = data[i].split(' = ')
-        result_map[key] = tuple(value.replace('(', '').replace(')', '').replace(',', '').split())
-    return path_data, result_map
+import os
+import sys
 
 
-def solution(puzzle_data: tuple[str, dict[str, tuple[str, str]]]) -> int:
-    data_path, data_map = puzzle_data
-    my_place = [point for point in data_map if point.endswith('A')]
-    start_point = my_place[0]
-    turn = 1
-    my_place[0] = data_map.get(my_place[0])[int(data_path[turn % len(data_path)])]
-    while my_place[0] != start_point:
-        # for k in range(len(my_place)):
-        my_place[0] = data_map.get(my_place[0])[int(data_path[turn % len(data_path)])]
-        turn += 1
-        # if len(list(filter(lambda x: x.endswith('Z'), my_place))) >= 3:
-        print(turn, my_place)
-    print(my_place)
-    return turn
+def create_base():
+    for i in range(1, 26):
+        path = f'day_{str(i).zfill(2)}'
+        if not os.path.exists(path):
+            os.mkdir(path)
+            file_names = ['__init__.py', 'test_data.txt', 'input_data.txt',
+                          'solve_01.py', 'solve_02.py']
+            for name in file_names:
+                with open(os.path.join(path, name), 'w', encoding='UTF-8') as file:
+                    file.write('')
 
 
-data = data_reader('day_08/input_data.txt')
-print(solution(data))
+if __name__ == '__main__':
+    create_base()
